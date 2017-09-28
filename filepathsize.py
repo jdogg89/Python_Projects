495d53e97e5341136e01f7985bdd4bbb07fd54f8
tec751 <justin.hauer@capitalone.com>
2017-09-28 11:10:28 -0500

    adding filesize.py file

---
+++ filepathsize.py
@@ -0,0 +1,19 @@
+# #!/usr/bin/python
+import os
+import json
+import sys
+try:
+    mount = str(sys.argv[1])
+except IndexError:
+    print ("Give mount point as arugments")
+    sys.exit()
+data = dict()
+for root, directories, filenames in os.walk(mount):
+    for filename in filenames:
+        file_path = os.path.join(root,filename)
+        file_size = os.path.getsize(file_path)
+        data.update({file_path:file_size})
+        #print file_path,file_size
+        #print (data)
+        j = json.dumps(data)
+        print (j)
